d91691132d1067a707ec53312693790f23252d45
Harshit <itsharshitraj@gmail.com>
2025-02-22 12:23:12 +0530

    Added UC3: Personalized Greeting

--- src/main/java/com/tit/greetingapp/controller/GreetingController.java
+++ src/main/java/com/tit/greetingapp/controller/GreetingController.java
@@ -4,6 +4,7 @@ import com.tit.greetingapp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +23,12 @@ public class GreetingController {
     @GetMapping("/service")
     public String sayHelloFromService() {
         return greetingService.getGreetingMessage();
+    }
+
+    // UC3 - Personalized Greeting
+    @GetMapping("/greet")
+    public String greetUser(@RequestParam(required = false) String firstName,
+                            @RequestParam(required = false) String lastName) {
+        return greetingService.getPersonalizedGreeting(firstName, lastName);
     }
 }
--- src/main/java/com/tit/greetingapp/service/GreetingService.java
+++ src/main/java/com/tit/greetingapp/service/GreetingService.java
@@ -8,4 +8,16 @@ public class GreetingService {
     public String getGreetingMessage() {
         return "Hello from Service Layer!";
     }
+
+    public String getPersonalizedGreeting(String firstName, String lastName) {
+        if (firstName == null && lastName == null) {
+            return "Hello World";
+        } else if (lastName == null) {
+            return "Hello " + firstName;
+        } else if (firstName == null) {
+            return "Hello " + lastName;
+        } else {
+            return "Hello " + firstName + " " + lastName;
+        }
+    }
 }
