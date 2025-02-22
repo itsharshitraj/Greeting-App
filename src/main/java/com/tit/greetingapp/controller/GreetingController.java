800ce537082918d040ba97c1ddc5cf89709bc870
Harshit <itsharshitraj@gmail.com>
2025-02-22 12:20:38 +0530

    Added UC2: Greeting using Service Layer

--- src/main/java/com/tit/greetingapp/controller/GreetingController.java
+++ src/main/java/com/tit/greetingapp/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package com.tit.greetingapp.controller;
 
+import com.tit.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class GreetingController {
+    @Autowired
+    private GreetingService greetingService;
 
     // UC1 - Basic Greeting Message
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    // UC2 - Use Service Layer to return Greeting Message
+    @GetMapping("/service")
+    public String sayHelloFromService() {
+        return greetingService.getGreetingMessage();
     }
 }
